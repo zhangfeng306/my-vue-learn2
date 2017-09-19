001d13d95c4a9249f825409e627c4199da2a00d4
张峰 <zhangfeng@iafclub.com>
2017-09-19 16:46:29 +0800

    提交，父子模块数据流传递 props

---
+++ src/components/child.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span class="h3" > 子界面 &nbsp;&nbsp; {{mess}}</span>
+     <labe class="col-sm-2 control-label">{{todo.text}}</labe>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        mess: '',
+        todo: {
+          text: '士大夫',
+          isComplete: false
+        }
+      }
+    },
+//    props: ['mess', 'todo']
+    props: {
+      mess: {
+        type: String,
+        default: 'mess'
+      },
+      todo: {
+        type: Object,
+        default: {
+          text: '士大夫',
+          isComplete: false
+        }
+      }
+    }
+  }
+</script>
--- src/page/index.vue
+++ src/page/index.vue
@@ -1,55 +1,73 @@
 <template>
   <div>
     <Header></Header>
-
-      <div class="container mt60">
-            <div class="row"   v-for="i in list">
-
-            <div class="col">
-              <time v-text="$utils.goodTime(i.create_at)"></time>
-            </div>
-            <div class="col">
-              <router-link :to="'/content/' + i.id">
-                {{ i.title }}
-              </router-link>
-            </div>
-            </div>
-
+    <div class="container mt60">
+      <input v-model="propsStr" placeholder="在我里面输入的数据会传递到子模块" class="form-control"> <br>
+      <Child :mess="propsStr" ></Child><br>
+      <Child mess="子页面数据传递props"></Child><br>
+      <Child  :todo="todo"></Child>
+      <div class="row" v-for="i in list">
+        <!--数据绑定动态获取父页面数据-->
+        <!--  <Child :mess="$utils.goodTime(i.create_at)"></Child>-->
+        <div class="col">
+          <time v-text="$utils.goodTime(i.create_at)"></time>
+        </div>
+        <div class="col">
+          <router-link :to="'/content/' + i.id">
+            {{ i.title }}
+          </router-link>
+        </div>
       </div>
+
+    </div>
 
     <Footer></Footer>
   </div>
 </template>
 <script>
-import Header from '../components/header.vue'
-import Footer from '../components/footer.vue'
+  import Header from '../components/header.vue'
+  import Footer from '../components/footer.vue'
+  import Child from '../components/child.vue'
 
-export default {
-  components: { Header, Footer },
-  data () {
-    return {
-      list: []
-    }
-  },
-  created () {
-    this.getData()
-  },
-  methods: {
-    getData () {
-      this.$api.get('topics', null, r => {
-        this.list = r.data
-      })
+  export default {
+    components: {
+      Header,
+      Footer,
+      Child
+    },
+    data () {
+      return {
+        list: [],
+        propsStr: '父页面数据',
+        todo: {
+          text: 'Learn Vue',
+          isComplete: false
+        }
+      }
+    },
+    created () {
+      this.getData()
+    },
+    methods: {
+      getData () {
+        this.$api.get('topics', null, r => {
+          this.list = r.data
+        })
+      }
     }
   }
-}
 </script>
 
 <style>
-.mt60 {
-  margin-top: 60px;
-}
-.navbar-brand {
-  font-size: 30px;
-  font-weight: 600;
-}
+  .form-control{
+    width: 30%
+  }
+  .mt60 {
+    margin-top: 60px;
+  }
+
+  .navbar-brand {
+    font-size: 30px;
+    font-weight: 600;
+  }
 </style>
